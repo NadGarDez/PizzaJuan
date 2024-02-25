65f67f5f46924784eed993aa18dec526d7d48284
Nad GarDez <iranad@hotmail.com>
2024-02-25 13:25:03 -0400

    finished user screen ui

--- sources/components/surfaces/TransformedSquareWithoutRotation.tsx
+++ sources/components/surfaces/TransformedSquareWithoutRotation.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Dimensions, StyleSheet, View } from "react-native";
 import { colors } from "../../styles/colors";
+import LinearGradient from "react-native-linear-gradient";
 
 
 const styles = StyleSheet.create(
     {
         square:{
             width: Dimensions.get("screen").width,
-            height:Dimensions.get("screen").width,
+            height:Dimensions.get("window").height * 0.5,
             position: "absolute",
             backgroundColor:colors.principal,
-            overflow: "hidden"
+            overflow: "hidden",
+            borderBottomLeftRadius:12,
+            borderBottomRightRadius:12
            
         },
         container: {
@@ -20,7 +23,7 @@ const styles = StyleSheet.create(
             top:0,
             height:Dimensions.get("window").height,
             width:Dimensions.get("screen").width,
-            zIndex:-2
+            zIndex:-3
         },
         circle: {
             width: Dimensions.get("screen").width * 2,
@@ -34,39 +37,47 @@ const styles = StyleSheet.create(
             height: Dimensions.get("screen").width * 2,
             borderRadius: Dimensions.get("screen").width,
             backgroundColor:colors.hightLightPrincipal + "90",
-        }   
+        },
+        linearGradient: {
+            position: "absolute",
+            top:0,
+            zIndex:-2,
+            width:"100%",
+            height: Dimensions.get("window").height,
+        }  
     }
 )
 
 export const TransformedSquareWithoutRotation = ():JSX.Element=> {
     return (
-        <View style={styles.container}>
-            <View style={[
-                styles.square,
-            ]}>
+        <>
+            <View style={styles.container}>
                 <View style={[
-                    styles.circle2,
-                    {
-                        transform: [
-                            {translateX:-(Dimensions.get("screen").width)}
-                        ]
-                    }
+                    styles.square,
                 ]}>
+                    <View style={[
+                        styles.circle2,
+                        {
+                            transform: [
+                                {translateX:-(Dimensions.get("screen").width)}
+                            ]
+                        }
+                    ]}>
 
-                </View>
-                <View style={[
-                    styles.circle,
-                    {
-                        transform:[
-                            {translateY: -(Dimensions.get("screen").width * 2)}
-                        ]
-                    }
-                
-                ]}>
+                    </View>
+                    <View style={[
+                        styles.circle,
+                        {
+                            transform:[
+                                {translateY: -(Dimensions.get("screen").width * 2)}
+                            ]
+                        }
+                    
+                    ]}>
 
+                    </View>
                 </View>
             </View>
-        </View>
-        
+        </>
     )
 }
