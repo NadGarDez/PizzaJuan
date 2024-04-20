2c616b587d305dca63292a82d7ec353afb6dee7b
Nad GarDez <iranad@hotmail.com>
2024-04-20 12:34:46 -0400

    added form to amout container

--- sources/components/surfaces/AmountContainer.tsx
+++ sources/components/surfaces/AmountContainer.tsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 import { colors } from "../../styles/colors";
 import { shadows } from "../../styles/shadow";
 
+const circleWidth = (Dimensions.get("screen").width - 32) 
+
 const styles = StyleSheet.create({
     container: {
-        position: 'relative',
-        borderRadius:10,
+        overflow: 'hidden',
         height:130,
-        backgroundColor: colors.principal,
+        borderRadius:10,
+        ...shadows.principalShadow
+    },
+    cover: {
+        position: 'absolute',
+        top:0,
+        backgroundColor:'transparent',
         display:"flex",
         paddingHorizontal:8,
         paddingVertical:16,
         zIndex:20,
-        overflow: 'hidden',
-        ...shadows.principalShadow
+        height:130,
+    },
+    form: {
+        backgroundColor: colors.principal,
+        height:130,
     },
     titleText: {
         fontSize:25,
@@ -29,13 +39,41 @@ const styles = StyleSheet.create({
     descriptionText: {
         fontSize:15,
         color:colors.white_card,
-        fontWeight:"300"
+        fontWeight:"400"
     },
     titleContainer: {
         paddingBottom:8
     },
     priceContainer: {
         flex:1
+    },
+    circle: {
+        width: circleWidth,
+        height: circleWidth,
+        borderRadius: circleWidth /2,
+        backgroundColor: colors.hightLightPrincipal + "60"
+    },
+    firstCircle: {
+        position: "absolute",
+        left: - (circleWidth/2),
+        backgroundColor: "#00000010"
+    },
+    secondCircle: {
+        position: "absolute",
+        right: - (circleWidth/2),
+        backgroundColor: colors.hightLightPrincipal + '80'
+    },
+    thirdCircle: {
+        position: "absolute",
+        right: - (circleWidth/2),
+        bottom:0,
+        backgroundColor: colors.hightLightPrincipal + '80'
+    },
+    fourthCircle: {
+        position: "absolute",
+        left: - (circleWidth/2),
+        bottom:0,
+        backgroundColor: "#00000010"
     }
 })
 
@@ -43,8 +81,15 @@ const styles = StyleSheet.create({
 
 export const AmountContainer = ()=> {
     return (
-        <>
-            <View style={styles.container}>
+        <View style={styles.container}>
+            <View style={styles.form}>
+                <View style={{...styles.circle, ...styles.firstCircle}}/>
+                <View style={{...styles.circle, ...styles.secondCircle}}/>
+                <View style={styles.circle}/>
+                <View style={{...styles.circle, ...styles.thirdCircle}}/>
+                <View style={{...styles.circle, ...styles.fourthCircle}}/>
+            </View>
+            <View style={styles.cover}>
                 <View style={styles.titleContainer}>
                     <Text style={styles.titleText}>
                         Pedido #1234abcd567
@@ -59,6 +104,6 @@ export const AmountContainer = ()=> {
                     </Text>
                 </View>
             </View>
-        </>
+        </View>
     )
 }
